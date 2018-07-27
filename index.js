0c041dc5a896f7a3a1fe297fbd6fd2e27f6a200f
Joe Muro <jmuro0321@gmail.com>
2018-07-27 17:20:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function shout(it) {
   return it.toUpperCase()
 }
+
+function whisper(it) {
+  return it.toLowerCase()
+}
+
+function logShout(it) {
+  console.log(it.toUpperCase())
+}
+
+function logWhisper(it) {
+  console.log(it.toLowerCase)
+}
+
